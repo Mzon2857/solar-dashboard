d784a755ad96192eb43729dc4ed38242d09d3f2b
Michael Deng <39012169+Mzon2857@users.noreply.github.com>
2023-09-30 00:21:45 -0400

    battery chart

--- solar-dashboard/src/components/batteryChart/batteryChart.tsx
+++ solar-dashboard/src/components/batteryChart/batteryChart.tsx
@@ -1,9 +1,78 @@
-import "./batteryChart.scss"
+import React, { PureComponent } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-const batteryChart = () => {
-    return (
-        <div className="batteryChart">Battery Chart</div>
-    )
-}
+import "./batteryChart.scss";
 
-export default batteryChart
+const data = [
+  {
+    ID: "1",
+    percent: 40,
+  },
+  {
+    ID: "2",
+    percent: 15,
+  },
+  {
+    ID: "3",
+    percent: 20,
+  },
+  {
+    ID: "4",
+    percent: 45,
+  },
+  {
+    ID: "5",
+    percent: 70,
+  },
+  {
+    ID: "6",
+    percent: 85,
+  },
+  {
+    ID: "7",
+    percent: 69,
+  },
+];
+
+const BatteryChart = () => {
+  return (
+    <div className="batteryChart">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="ID" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="percent"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default BatteryChart;
--- solar-dashboard/src/pages/Home/Home.tsx
+++ solar-dashboard/src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import './home.scss'
+import BatteryChart from "../../components/batteryChart/batteryChart.tsx"
 
 const Home = () => {
     return (
         <div className="home">
             <div className="box box1">Misc analytics</div>
             <div className="box box2">Data stream input</div>
-            <div className="box box3">Battery chart</div>
+            <div className="box box3"><BatteryChart/></div>
             <div className="box box4">Energy Use chart</div>
             <div className="box box5">Energy Use analytics</div>
             <div className="box box6">ALERTS</div>
